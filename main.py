fa986e2e52835c25eac81547ad0b1f448a0427e5
Johan154 <80755015+Johan154@users.noreply.github.com>
2023-11-25 20:12:22 +0100

    Check row index

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def check_cross():
+    return 0
+
+
+def check_items(in_sud):
+    for row in in_sud:
+        for sub_row in row:
+            for i in sub_row:
+                if i > 0:
+                    print(i, in_sud.index(row))
+    return 0
+
+
 if __name__ == '__main__':
     start = \
         [[[5, 3, 0], [0, 7, 0], [0, 0, 0]],
@@ -9,3 +22,4 @@ if __name__ == '__main__':
          [[0, 6, 0], [0, 0, 0], [2, 8, 0]],
          [[0, 0, 0], [4, 1, 9], [0, 0, 5]],
          [[0, 0, 0], [0, 8, 0], [0, 7, 9]]]
+    check_items(start)
