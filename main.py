3bde7cdf63eac738c4b36ae43697c76fb2d327b3
Johan154 <80755015+Johan154@users.noreply.github.com>
2023-11-28 18:49:34 +0100

    Calculate initial options list rows, columns and squares

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import math
+import sys
 
 
 def check_cross(input_sudoku, row_idx, column_idx):
-    print(row_idx, column_idx)
+    # print(row_idx, column_idx)
     # TODO: automatically generate this array
     option_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for sub_row in input_sudoku[row_idx - 1]:
@@ -17,8 +18,16 @@ def check_cross(input_sudoku, row_idx, column_idx):
     return option_list
 
 
-def check_square():
-    return 0
+def check_square(input_sudoku, row_idx, column_idx):
+    square_row_idx = math.floor(row_idx * 0.33)
+    square_col_idx = math.floor((column_idx - 1) / 3)
+
+    option_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in range(square_row_idx * 3, (square_row_idx * 3) + 3):
+        for j in input_sudoku[i][square_col_idx]:
+            if j > 0 and j in option_list:
+                option_list.remove(j)
+    return option_list
 
 
 def check_items(in_sud, in_opt):
@@ -29,10 +38,13 @@ def check_items(in_sud, in_opt):
                 if i == 0:
                     row_index = in_sud.index(row) + 1
                     column_index = ((row.index(sub_row)) * 3) + 1 + count
-                    item_options = check_cross(in_sud, row_index, column_index)
-                    print(in_sud.index(row), row.index(sub_row), count, item_options)
-                    in_opt[in_sud.index(row)][row.index(sub_row)][count] = item_options
+                    item_options_iter1 = check_cross(in_sud, row_index, column_index)
+                    item_options_iter2 = check_square(in_sud, row_index, column_index)
+                    in_opt[in_sud.index(row)][row.index(sub_row)][count] = list(set(item_options_iter1) & set(item_options_iter2))
+                    # sys.exit()
                 count += 1
+    # for rowx in in_opt:
+    #     print(rowx)
     return 0
 
 
