3be180faeb9c523f4bdf6410068001ba8704fd11
Johan154 <80755015+Johan154@users.noreply.github.com>
2023-12-02 15:41:57 +0100

    Redesigned approach check cross and square

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import math
 import copy
+import sys
 
 from collections import Counter
 
 
-def check_cross(input_sudoku, row_idx, column_idx):
+def check_straight(input_list):
     option_list = [i for i in range(1, 10)]
-    for sub_row in input_sudoku[row_idx - 1]:
-        for i in sub_row:
-            if i > 0:
-                option_list.remove(i)
-    sub_row_idx = math.floor((column_idx - 1) / 3) % 3
-    col = [row[sub_row_idx][(column_idx - 1) % 3] for row in input_sudoku]
-    for j in col:
-        if j > 0 and j in option_list:
+    for j in option_list:
+        if j > 0:
             option_list.remove(j)
     return option_list
 
 
-def check_square(input_sudoku, row_idx, column_idx):
-    square_row_idx = math.floor(row_idx * 0.33)
-    square_col_idx = math.floor((column_idx - 1) / 3)
-
-    option_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def check_square(input_sudoku, row_idx, col_idx):
+    option_list = [i for i in range(1, 10)]
+    square_row_idx = math.floor(row_idx / 3)
+    square_col_idx = math.floor(col_idx / 3)
     for i in range(square_row_idx * 3, (square_row_idx * 3) + 3):
-        for j in input_sudoku[i][square_col_idx]:
+        # print(i, input_sudoku[i][(square_col_idx * 3):(square_col_idx * 3) + 3])
+        for j in input_sudoku[i][square_row_idx:square_row_idx + 3]:
             if j > 0 and j in option_list:
                 option_list.remove(j)
     return option_list
@@ -83,43 +78,51 @@ def check_options(input_sudoku, input_options):
 
 def check_items(input_sudoku, input_options):
     for row in input_sudoku:
-        for sub_row in row:
-            count = 0
-            for i in sub_row:
-                if i == 0:
-                    row_index = input_sudoku.index(row) + 1
-                    column_index = ((row.index(sub_row)) * 3) + 1 + count
-                    item_options_iter1 = check_cross(input_sudoku, row_index, column_index)
-                    item_options_iter2 = check_square(input_sudoku, row_index, column_index)
-                    input_options[input_sudoku.index(row)][row.index(sub_row)][count] = list(set(item_options_iter1) & set(item_options_iter2))
-                count += 1
-    out_sudoku = check_options(input_sudoku, input_options)
-    return out_sudoku, input_options
+        row_idx = input_sudoku.index(row)
+        col_idx = 0
+        for item in row:
+            if item == 0:
+                row_options = check_straight(row)
+                col = [line[col_idx] for line in input_sudoku]
+                col_options = check_straight(col)
+                square_options = check_square(input_sudoku, row_idx, col_idx)
+
+                # TODO: get union of the 3 lists
+                # sys.exit()
+            col_idx += 1
+
+    # out_sudoku = check_options(input_sudoku, input_options)
+    # return out_sudoku, input_options
 
 
 if __name__ == '__main__':
     # TODO: read directly from input file
+    # Create initial sudoku
     start = \
-        [[[0, 6, 0], [0, 0, 0], [0, 9, 1]],
-         [[0, 2, 8], [7, 0, 0], [0, 0, 3]],
-         [[1, 0, 0], [4, 0, 0], [8, 0, 0]],
-         [[0, 0, 0], [5, 0, 0], [0, 0, 2]],
-         [[5, 0, 0], [2, 0, 0], [0, 0, 4]],
-         [[0, 3, 0], [0, 0, 6], [0, 0, 7]],
-         [[0, 7, 0], [0, 0, 0], [0, 0, 0]],
-         [[0, 1, 0], [0, 8, 0], [6, 0, 0]],
-         [[3, 0, 4], [0, 2, 0], [0, 0, 0]]]
+        [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+         [6, 0, 0, 1, 9, 5, 0, 0, 0],
+         [0, 9, 8, 0, 0, 0, 0, 6, 0],
+         [8, 0, 0, 0, 6, 0, 0, 0, 3],
+         [4, 0, 0, 8, 0, 3, 0, 0, 1],
+         [7, 0, 0, 0, 2, 0, 0, 0, 6],
+         [0, 6, 0, 0, 0, 0, 2, 8, 0],
+         [0, 0, 0, 4, 1, 9, 0, 0, 5],
+         [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
-    options = [[[[] for k in range(3)] for j in range(3)] for i in range(9)]
+    # Create initial options list
+    options = [[[] for j in range(9)] for i in range(9)]
+    # for row in options:
+    #     print(row)
 
-    start_sud = copy.deepcopy(start)
-    in_sud = start
-    in_opt = []
-    out_sud, out_opt = check_items(in_sud, options)
+    # start_sud = copy.deepcopy(start)
+    # in_sud = start
+    # in_opt = []
+    # out_sud, out_opt = check_items(in_sud, options)
 
-    while out_sud != start_sud:
-        start_sud = copy.deepcopy(out_sud)
-        out_sud, out_opt = check_items(out_sud, out_opt)
+    check_items(start, options)
+    # while out_sud != start_sud:
+    #     start_sud = copy.deepcopy(out_sud)
+    #     out_sud, out_opt = check_items(out_sud, out_opt)
 
-    for y in out_opt:
-        print(y)
+    # for y in out_opt:
+    #     print(y)
